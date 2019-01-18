10186145fb50c0d6ec3bece44c088812ac0a25c7
Arunscape <8227297+Arunscape@users.noreply.github.com>
2019-01-17 23:05:49 -0700

    nice

---
+++ MemeBot.py
@@ -0,0 +1,58 @@
+import os
+import random
+from glob import glob
+from shutil import move
+from slackclient import SlackClient
+from json import dumps as jsondump
+from config import API_TOKEN, CHANNEL, MEMES_DIR, IMG_EXTS
+sc = SlackClient(API_TOKEN)
+
+
+class MemeBot:
+
+    def __init__(self):
+        self.images = []
+
+    def getImages(self):
+        self.images = []
+        for ext in IMG_EXTS:
+            self.images.extend(glob(MEMES_DIR + ext))
+
+    def mv_image(self, img):
+        move(img, './posted_memes/' +
+             os.path.normpath(img).replace(os.path.normpath(MEMES_DIR), ''))
+        self.images.remove(img)
+
+    def getRandomImage(self):
+        return random.choice(self.images)
+
+    def sendMeme(self, img):
+        response = sc.api_call(
+            'files.upload',
+            channels=CHANNEL,
+            file=open(img, 'rb')
+        )
+        if response['ok']:
+            self.mv_image(img)
+        else:
+            self.debugPrint(jsondump(response, indent=4),
+                            "ERROR POSTING MEME 😭")
+
+    def sendMessage(self, msg):
+        sc.api_call(
+            'chat.postMessage',
+            channel=CHANNEL,
+            text=msg
+        )
+
+    def debugPrint(self, str, typeoferr):
+        print("\n👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀")
+        print(typeoferr)
+        print(str)
+        print("👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀👀\n")
+
+
+memebot = MemeBot()
+memebot.getImages()
+memebot.sendMeme(memebot.getRandomImage())
+# sendMessage("👀")
