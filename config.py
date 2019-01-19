946c6bd2cbb844643a9bc14aa9d29faf0761908e
Arun Woosaree <8227297+Arunscape@users.noreply.github.com>
2019-01-18 19:00:25 -0700

    edit online because I'm lazy

--- config.py
+++ config.py
@@ -5,3 +5,7 @@ CHANNEL = 'meme-bot-test'
 MEMES_DIR = './memes/'
 
 IMG_EXTS = ('*.png', '*.jpg', '*.img', '*.gif')
+
+if __name__ == "__main__":
+    from util import critError
+    critError("EXECUTE main.py NOT THIS FILE")
